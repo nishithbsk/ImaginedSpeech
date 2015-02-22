4c12db61760988cec93ad19eea86e2cb2a8be5e9
Nishith Khandwala <nishith@stanford.edu>
2015-02-21 18:29:26 -0800

    Implemented getClassSamples. Not tested

--- scripts/data-interface.py
+++ scripts/data-interface.py
@@ -1,7 +1,15 @@
 import scipy as sp
 import numpy as np
+import os
 
 timings = {'ha_START' = ??, 'ha_END' = ??, 'bit_START' = ??, 'bit_END' = ??, 'habit_START' = ??, 'habit_END' = ??, 'OVERLAP' = 100}
+
+HA_PATH = '../data/syllables/ha/'
+BIT_PATH = '../data/syllables/bit/'
+HABIT_PATH = '../data/syllables/habit/'
+SIG_PATH = '../data/syllables/sig/'
+NAL_PATH = '../data/syllables/nal/'
+SIGNAL_PATH = '../data/syllables/signal/'
 
 # Input: string of file name
 # Return: 2d array with row a channel and column as reading
@@ -33,5 +41,23 @@ def getClassSamples(className, preprocessfn = None):
 	# 	return array
 	# return preprocessed array of npmatrices of all class samples
 	# return array of npmatrices of all class samples
+	all_samples = []
+	# Getting all MAT files
+	# Neglecting the first file (.DS_Store)
+	CLASSPATH = eval(className + 'PATH')
+
+	all_MAT_files = os.listdir(CLASSPATH)[1:]
+	for MAT_file in all_MAT_files:
+		sample_path = CLASSPATH + MAT_file
+		sample_matrix = matFileTo2dMatrix(sample_path)
+		if preprocessfn:
+			sample_matrix = preprocessfn(sample_matrix)
+		all_samples.append(sample_matrix)
+
+	return np.array(all_samples)
+
+
+
+
 
 
