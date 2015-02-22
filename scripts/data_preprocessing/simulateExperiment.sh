2689df6e7a69701e8e4ddf40c4eae9847af79ea3
Nishith Khandwala <nishith@stanford.edu>
2015-02-21 17:13:40 -0800

    Added simulateExperiment.sh

---
+++ scripts/data_preprocessing/simulateExperiment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for i in {1..10}
+do
+	index=$RANDOM
+	index=$[ $index % 2]
+	index=$((index + 1)) 
+	if (( $index == 1 ))
+	then
+		echo "low beep"
+		afplay ../../data/data_preprocessing/300Hz.wav
+	else
+		echo "high beep"
+		afplay ../../data/data_preprocessing/900Hz.wav
+	fi		
+	sleep 2.3
+done
