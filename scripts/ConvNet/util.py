35fcd0c1a4930a176e4d4283afc03001eb16d9a4
Nishith Khandwala <nishith@stanford.edu>
2015-03-21 01:58:57 -0700

    Added loss and acc visualization code

--- scripts/ConvNet/util.py
+++ scripts/ConvNet/util.py
@@ -6,3 +6,19 @@ PATH = '../../data/'
 def get_all_instances_of_symbol(symbol):
 	f = open(PATH + symbol.upper(), 'rb')
 	return pickle.load(f)
+
+def plot(loss_history, train_acc_history, val_acc_history):
+	plt.subplot(2, 1, 1)
+	plt.plot(train_acc_history)
+	plt.plot(val_acc_history)
+	plt.title('accuracy vs time')
+	plt.legend(['train', 'val'], loc=4)
+	plt.xlabel('epoch')
+	plt.ylabel('classification accuracy')
+
+	plt.subplot(2, 1, 2)
+	plt.plot(loss_history)
+	plt.title('loss vs time')
+	plt.xlabel('iteration')
+	plt.ylabel('loss')
+	plt.show()
